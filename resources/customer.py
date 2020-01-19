68dfdd6a88d8f962ee98025ce3a9f59d817778cf
Krishnatheja Vanka <theja.vanka@gmail.com>
2020-01-19 16:22:34 +0530

    getCustomers completed

--- resources/customer.py
+++ resources/customer.py
@@ -1,9 +1,20 @@
 # Flast RESTFull Dependencies
 from flask_restful import Resource, reqparse
 from models.customer import CustomerMaster
+import collections
 
 # Customer Master API Scaffold
 class CustomerMasterAPI(Resource):
 
     def get(self):
-        return [dict(row) for row in CustomerMaster.objects().all()], 200
+        queryresult = [dict(row) for row in CustomerMaster.objects().all()]
+        grouped = collections.defaultdict(list)
+        for item in queryresult:
+            grouped[item['labels']].append(item)
+        result = []
+        for model, group in grouped.items():
+            resultdict = {}
+            resultdict['label'] =  model
+            resultdict['customers'] = group
+            result.append(resultdict)
+        return result, 200
