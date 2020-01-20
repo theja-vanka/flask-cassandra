0af241d78e416104cf28f59c9f18638fdc74547e
Krishnatheja Vanka <theja.vanka@gmail.com>
2020-01-20 20:20:29 +0530

    Version 1 Complete

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from flask_restful import Api
 from resources.customer import CustomerMasterAPI
 from resources.customer import CustomerCreateAPI
 from resources.cluster import NewClusterAPI
+from resources.cluster import ClusterPropAPI
 from resources.items import ItemMasterAPI
 from resources.transaction import TransactionMasterPopularityAPI
 from resources.transaction import TransactionMasterCollaboratorAPI
@@ -30,13 +31,14 @@ api.add_resource(HelloAPI,'/') #http://127.0.0.1:8080/
 api.add_resource(CustomerMasterAPI, '/getCustomers') #http://127.0.0.1:8080/getCustomers
 api.add_resource(CustomerCreateAPI, '/createCluster') #http://127.0.0.1:8080/createCluster
 api.add_resource(NewClusterAPI,'/getNewClusters') #http://127.0.0.1:8080/getNewClusters
+api.add_resource(ClusterPropAPI,'/getNewClusterprop') #http://127.0.0.1:8080/getNewClusterprop
 api.add_resource(ItemMasterAPI, '/getItems') #http://127.0.0.1:8080/getItems
 api.add_resource(TransactionMasterPopularityAPI, '/popularityModel') #http://127.0.0.1:8080/popularityModel
 api.add_resource(TransactionMasterCollaboratorAPI, '/collaborativeModel') #http://127.0.0.1:8080/collaborativeModel
 api.add_resource(TransactionMasterSellerAPI, '/getSelling') #http://127.0.0.1:8080/getSelling
 api.add_resource(ItemMadhuramMasterAPI, '/getMadhuramItems') #http://127.0.0.1:8080/getMadhuramitems
 api.add_resource(TransactionMadhuramMasterPopularityAPI, '/popularityMadhuramModel') #http://127.0.0.1:8080/popularityMadhuramModel
-api.add_resource(TransactionMadhuramMasterSellerAPI, '/sellerMadhuramModel') #http://127.0.0.1:8080/popularityMadhuramModel
+api.add_resource(TransactionMadhuramMasterSellerAPI, '/sellerMadhuramModel') #http://127.0.0.1:8080/sellerMadhuramModel
 
 
 # Main Function for app
--- resources/cluster.py
+++ resources/cluster.py
@@ -1,8 +1,9 @@
 # Flast RESTFull Dependencies
 from flask_restful import Resource, reqparse
-
+import flask
 # Model dependencies
 from models.cluster import NewClusterMaster
+from helpers.cassandradb import CassandraSession
 
 class NewClusterAPI(Resource):
 
@@ -10,3 +11,21 @@ class NewClusterAPI(Resource):
         # Return all clusters created by user
         result = [dict(row) for row in NewClusterMaster.objects().all()]
         return result, 200
+
+class ClusterPropAPI(Resource):
+    
+    def get(self):
+        data = {}
+        data['clusterid'] = flask.request.args.get('clusterid')
+        count = NewClusterMaster.objects().count() + 1 
+        cassObj = CassandraSession()
+        asyncquery = cassObj.session.execute_async('SELECT MAX(poi) FROM poi_frame ;')
+        maxvalue = list(asyncquery.result())[0]['system.max(poi)']
+        print(maxvalue)
+        asyncquery = cassObj.session.execute_async('SELECT * FROM poi_frame WHERE customer_label ='+data['clusterid'])
+        queryresult = list(asyncquery.result())
+        print(queryresult)
+        returndict = {}
+        returndict['probability'] = (maxvalue - queryresult[0]['poi']) / maxvalue
+        returndict['customer_label'] = queryresult[0]['customer_label']
+        return returndict, 200
